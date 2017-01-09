fe0b45da859dc8972cc478f1b0b2d325b953c943
Shahzad Khan <shahzadkhan3iii7@gmail.com>
2017-01-08 18:44:40 -0600

    Having a good day?

--- routes/forms.js
+++ routes/forms.js
@@ -4,75 +4,75 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-router.post('/adult', (req, res, next) =>{
-  console.log('post to adult');
-  if(req.body.pay === 'now'){ // if they are paying now, include paid option in database submission
-    console.log(req.body);
-    res.send(req.body);
-  }else{
-    console.log('pay later',req.body);
-    knex('userlog').insert({ //submit database information for dashboard
-      'firstName' : req.body['first-name'],
-      'lastName' : req.body['last-name'],
-      'appointment-time' : req.body['appointment-time'],
-      'paid' : 'Not Paid'
-    })
-    .returning('*').then((info) => {
-      let response = {};
-      console.log(info);
-      res.status(200);
-      res.send(info);
-    });
-  }
+router.post('/adult', (req, res, next) => {
+    console.log('post to adult');
+    if (req.body.pay === 'now') { // if they are paying now, include paid option in database submission
+        console.log(req.body);
+        res.send(req.body);
+    } else {
+        console.log('pay later', req.body);
+        knex('userlog').insert({ //submit database information for dashboard
+                'firstName': req.body['first-name'],
+                'lastName': req.body['last-name'],
+                'appointment-time': req.body['appointment-time'],
+                'paid': 'Not Paid'
+            })
+            .returning('*').then((info) => {
+                let response = {};
+                console.log(info);
+                res.status(200);
+                res.send(info);
+            });
+    }
 });
 
-router.post('/child', (req, res, next) =>{
-  console.log('post to child');
-  if(req.body.pay === 'now'){ // if they are paying now, include paid option in database submission
-    console.log(req.body);
-    res.send(req.body);
-  }else{
-    console.log('pay later',req.body);
-    knex('userlog').insert({ //submit database information for dashboard
-      'firstName' : req.body['child-first-name'],
-      'lastName' : req.body['child-last-name'],
-      'appointment-time' : req.body['appointment-time'],
-      'paid' : 'Not Paid'
-    })
-    .returning('*').then((info) => {
-      let response = {};
-      console.log(info);
-      res.status(200);
-      res.send(info);
-    });
-  }
+router.post('/child', (req, res, next) => {
+    console.log('post to child');
+    if (req.body.pay === 'now') { // if they are paying now, include paid option in database submission
+        console.log(req.body);
+        res.send(req.body);
+    } else {
+        console.log('pay later', req.body);
+        knex('userlog').insert({ //submit database information for dashboard
+                'firstName': req.body['child-first-name'],
+                'lastName': req.body['child-last-name'],
+                'appointment-time': req.body['appointment-time'],
+                'paid': 'Not Paid'
+            })
+            .returning('*').then((info) => {
+                let response = {};
+                console.log(info);
+                res.status(200);
+                res.send(info);
+            });
+    }
 });
 
-router.get('/dashboard', (req, res, next) =>{
-  knex('userlog').orderBy('created_at', 'desc')
-  .then((userInfo) => {
-    console.log('u',userInfo);
-    let sendInfo = [];
-    for (var i = 0; i < userInfo.length; i++) {
-      var today = new Date();
-      today = today.toDateString();
-      var checkInTime = userInfo[i]['created_at'].toDateString();
-      if(today === checkInTime){
-        var appointmentTime = tConvert(userInfo[i]['appointment-time']);
-        var parsedInfo = {};
-        parsedInfo['firstName'] = userInfo[i].firstName ;
-        parsedInfo['lastName'] = userInfo[i].lastName;
-        parsedInfo['appointment-time'] = appointmentTime;
-        parsedInfo['created_at'] = userInfo[i]['created_at'].toLocaleTimeString();
-        parsedInfo['paid'] = userInfo[i].paid;
-        sendInfo.push(parsedInfo);
-    }
-    }
-    console.log('p',sendInfo);
-    res.render('FrontEnd/index', {
-      data: sendInfo,
-    });
-  })
+router.get('/dashboard', (req, res, next) => {
+    knex('userlog').orderBy('created_at', 'desc')
+        .then((userInfo) => {
+            console.log('u', userInfo);
+            let sendInfo = [];
+            for (var i = 0; i < userInfo.length; i++) {
+                var today = new Date();
+                today = today.toDateString();
+                var checkInTime = userInfo[i]['created_at'].toDateString();
+                if (today === checkInTime) {
+                    var appointmentTime = tConvert(userInfo[i]['appointment-time']);
+                    var parsedInfo = {};
+                    parsedInfo['firstName'] = userInfo[i].firstName;
+                    parsedInfo['lastName'] = userInfo[i].lastName;
+                    parsedInfo['appointment-time'] = appointmentTime;
+                    parsedInfo['created_at'] = userInfo[i]['created_at'].toLocaleTimeString();
+                    parsedInfo['paid'] = userInfo[i].paid;
+                    sendInfo.push(parsedInfo);
+                }
+            }
+            console.log('p', sendInfo);
+            res.render('FrontEnd/index', {
+                data: sendInfo,
+            });
+        })
 });
 // router.post('/items', (req, res) => {
 //   console.log('HEADERS', req.headers);
@@ -99,16 +99,16 @@ router.get('/dashboard', (req, res, next) =>{
 //   });
 // });
 
-function tConvert (time) {
-  // Check correct time format and split into components
-  time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+function tConvert(time) {
+    // Check correct time format and split into components
+    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
-  if (time.length > 1) { // If time format correct
-    time = time.slice (1);  // Remove full string match value
-    time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
-    time[0] = +time[0] % 12 || 12; // Adjust hours
-  }
-  return time.join (''); // return adjusted time or original string
+    if (time.length > 1) { // If time format correct
+        time = time.slice(1); // Remove full string match value
+        time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+        time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join(''); // return adjusted time or original string
 }
 
 module.exports = router;
