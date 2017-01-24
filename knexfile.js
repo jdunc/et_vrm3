457d8ddd587511d016a754e16d3d17baddee5916
Jordan Duncan <j.duncan@rochester.edu>
2017-01-24 16:08:46 -0600

    update db

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,33 @@
+// module.exports = {
+//     development: {
+//         client: 'pg',
+//         connection: 'postgres://localhost/vrm_dev'
+//     },
+//     production: {
+//         client: 'pg',
+//         connection: process.env.DATABASE
+//     }
+// }
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: 'postgres://localhost/vrm_dev'
-    },
-    production: {
-        client: 'pg',
-        connection: process.env.HEROKU_POSTGRESQL_CYAN_URL
-    }
-}
+
+ development: {
+   client: 'pg',
+   connection: {
+     database: 'vrm_dev',
+     host: 'localhost'
+   }
+ },
+
+ test: {
+   client: 'pg',
+   connection: {
+     database: 'vrm_test',
+     host: 'localhost',
+   }
+ },
+
+ production: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL,
+ }
+ }
